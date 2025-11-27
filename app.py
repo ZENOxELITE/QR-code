29479222890c31a651917350b1f53a2de2e6fa7d
Muneer Ahmed <156942752+ZENOxELITE@users.noreply.github.com>
2025-11-27 15:46:13 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+# import qrcode as qr
+
+# img = qr.make('https://www.youtube.com/@wscubetech')
+
+# img.save('youtube.png')
+
+import streamlit as st
+import qrcode
+from PIL import Image
+import io
+
+st.title("Welcome to QR Code Generator")
+
+ask = st.text_input("Enter the link")
+
+if st.button("Generate QR Code"):
+
+    # Create QR Code
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=3,
+    )
+
+    qr.add_data(ask.strip())
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="white", back_color="black")
+
+    # Convert PIL image to bytes
+    buf = io.BytesIO()
+    img.save(buf, format="PNG")
+    byte_img = buf.getvalue()
+
+    st.success("QR Code created successfully!")
+
+    # Show QR on screen
+    st.image(byte_img, caption="Generated QR Code")
+
+    # Download button
+    st.download_button(
+        label="Download QR Code",
+        data=byte_img,
+        file_name="qrcode.png",
+        mime="image/png"
+    )
